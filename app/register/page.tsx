63e8c53f8236ed7a0dfda50275c67f17854c26fc
akhilachiju <akhilac.benzer@gmail.com>
2025-11-14 22:23:49 +0100

    updated register page

---
+++ app/register/page.tsx
@@ -0,0 +1,100 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Password do not match");
+      return;
+    }
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error || "Registration failed");
+      }
+
+      console.log(data);
+      router.push("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-sm border p-6 rounded-lg shadow-md"
+      >
+        <h2 className="text-xl font-semibold mb-4 text-center">Register</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full p-2 border rounded mb-3"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-2 border rounded mb-3"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          className="w-full p-2 border rounded mb-4"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
+        >
+          Register
+        </button>
+           <div className="text-center mt-4">
+        <p className="text-sm">
+          Already have an account?{" "}
+          <Link
+            href="/login"
+            className="text-blue-600 font-medium hover:underline"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+      </form>
+   
+    </div>
+  );
+}
+
+export default RegisterPage;
